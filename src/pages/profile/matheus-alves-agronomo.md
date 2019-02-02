9e40550bc741c769eca0d21e65620dfc7d8a5ddb
Vinicius Lemes <vinicius.lemes.silva@gmail.com>
2019-02-02 14:42:32 +0100

    feat: [wip] profile page

---
+++ src/pages/profile/matheus-alves-agronomo.md
@@ -0,0 +1,28 @@
+---
+templateKey: profile-page
+name: Matheus Alves
+job: Engenheiro Agrônomo
+university: Unesp
+graduationYear: 2010
+avatar: /img/man-avatar.png
+skills:
+  - Pastagem
+  - Forrageiras
+region: Uberaba
+---
+
+#### Interação na plataforma
+
+- Clientes atendidos: 6
+- Número de contatos recebidos: 10
+- Tempo médio de resposta: 1 hora
+
+#### Experiência
+
+- 2 anos com vendas
+- 5 anos com controle biológico
+- 2 anos com forragem
+
+### Avaliação de clientes
+
+> _"O Matheus me auxiliou a estabelecer os piques e no planejamento de rotação. Me explicou a importância da adubação nos pastos"_ > **José**
---
+++ src/templates/profile-page.js
@@ -0,0 +1,131 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Helmet from "react-helmet";
+import { graphql } from "gatsby";
+import Layout from "../components/Layout";
+import Content, { HTMLContent } from "../components/Content";
+
+export const ProfileTemplate = ({
+  content,
+  contentComponent,
+  avatar,
+  name,
+  job,
+  university,
+  graduationYear,
+  skills = [],
+  region,
+  helmet
+}) => {
+  const ProfileContent = contentComponent || Content;
+
+  return (
+    <section className="section">
+      {helmet || ""}
+      <div className="container content">
+        <div className="columns">
+          <div className="column has-text-centered">
+            <figure className="image">
+              <img
+                className="is-rounded"
+                alt="avatar"
+                src={avatar.childImageSharp.fluid.src}
+              />
+            </figure>
+            <div className="is-size-2">{name}</div>
+            <div>
+              <span className="is-size-5">{job}</span>
+              <div className="has-text-grey">
+                {university} <span className="is-size-7">{graduationYear}</span>
+              </div>
+            </div>
+            <div className="column">
+              <div className="has-text-info is-size-7">Especialidades</div>
+              <div className="tags" style={{ justifyContent: "center" }}>
+                {skills.map(skill => (
+                  <span key={skill} className="tag">
+                    {skill}
+                  </span>
+                ))}
+              </div>
+            </div>
+            <div className="column">
+              <div className="has-text-info is-size-7">Região</div>
+              <span className="tag">{region}</span>
+            </div>
+          </div>
+          <div className="column is-paddingless">
+            <ProfileContent content={content} />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+ProfileTemplate.propTypes = {
+  content: PropTypes.node.isRequired,
+  contentComponent: PropTypes.func,
+  helmet: PropTypes.object,
+  name: PropTypes.string.isRequired,
+  job: PropTypes.string.isRequired,
+  university: PropTypes.string.isRequired,
+  skills: PropTypes.array.isRequired
+};
+
+const Profile = ({ data }) => {
+  const { markdownRemark: profile } = data;
+
+  const description = `${profile.frontmatter.name} - ${
+    profile.frontmatter.job
+  }`;
+
+  return (
+    <Layout>
+      <ProfileTemplate
+        content={profile.html}
+        contentComponent={HTMLContent}
+        helmet={
+          <Helmet titleTemplate="%s">
+            <title>{`${description}`}</title>
+            <meta name="description" content={`${description}`} />
+          </Helmet>
+        }
+        {...profile.frontmatter}
+      />
+    </Layout>
+  );
+};
+
+Profile.propTypes = {
+  data: PropTypes.shape({
+    markdownRemark: PropTypes.object
+  })
+};
+
+export default Profile;
+
+export const pageQuery = graphql`
+  query ProfileByID($id: String!) {
+    markdownRemark(id: { eq: $id }) {
+      id
+      html
+      frontmatter {
+        avatar {
+          childImageSharp {
+            fluid(maxWidth: 400, maxHeight: 400) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+        name
+        job
+        university
+        skills
+        university
+        graduationYear
+        region
+      }
+    }
+  }
+`;
