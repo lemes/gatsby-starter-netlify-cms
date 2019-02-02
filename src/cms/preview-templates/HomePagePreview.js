6b514d86aba77ca7a6fa1ba16bf6d7cfb829d08d
Vinicius Lemes <vinicius.lemes.silva@gmail.com>
2019-02-02 21:33:48 +0100

    fix: home admin preview

--- src/cms/preview-templates/HomePagePreview.js
+++ src/cms/preview-templates/HomePagePreview.js
@@ -1,12 +1,16 @@
 import React from "react";
 import { HomePageTemplate } from "../../templates/home-page";
 
-const HomePagePreview = ({ entry }) => (
-  <HomePageTemplate
-    title={entry.getIn(["data", "title"])}
-    subtitle={entry.getIn(["data", "subtitle"])}
-    features={entry.getIn(["data", "features"])}
-  />
-);
+const HomePagePreview = ({ entry }) => {
+  const featuresEntry = entry.getIn(["data", "features"]);
+  const features = featuresEntry ? featuresEntry.toJS() : [];
+  return (
+    <HomePageTemplate
+      title={entry.getIn(["data", "title"])}
+      subtitle={entry.getIn(["data", "subtitle"])}
+      features={features}
+    />
+  );
+};
 
 export default HomePagePreview;
