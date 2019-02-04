34532938b18c2624d2dc33f261ee8e24ebb7ebc4
Vinicius Lemes <vinicius.lemes.silva@gmail.com>
2019-02-04 19:53:49 +0100

    feat: contact thank message

--- src/pages/contact/thanks.js
+++ src/pages/contact/thanks.js
@@ -6,8 +6,13 @@ export default () => (
     <section className="section">
       <div className="container">
         <div className="content">
-          <h1>Obrigado!</h1>
-          <p>This is a custom thank you page for form submissions</p>
+          <h1>Obrigado pelo contato!</h1>
+          <p>
+            Em breve retornaremos a sua mensagem.
+            <br />
+            <br />
+            Equipe Agro Comunidade
+          </p>
         </div>
       </div>
     </section>
